34e4930f8188278b5c275926135122505f2bd220
ADS2011001 <ADS2011001@acad.local>
2023-05-13 08:30:43 -0300

    atividade

---
+++ Subtracao.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+
+img1 = cv2.imread('./images/anime/tanjiro.jpg')
+img2 = cv2.imread('./images/anime/inosuke.jpg')
+img4 = img1
+img5 = img1
+
+img1 = cv2.cvtColor(img1, cv2.COLOR_RGB2GRAY)
+img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2GRAY)
+
+cv2.imshow('Imagem original tanjiro', img1)
+cv2.imshow('Imagem original inosuke', img2)
+
+cv2.moveWindow("Imagem original tanjiro", 0,0)
+cv2.moveWindow("Imagem original inosuke", img2.shape[1] + 10, 0)
+
+# img3 = cv2.add(img1, img2)
+img3 = cv2.subtract(img1, img2)
+
+for x in range(0, img1.shape[0]):
+    for y in range(0, img1.shape[1]):
+        if((img1[x, y] - img2[x, y]) < 0):
+            img4[x, y] = 0
+        else:
+            img4[x, y] = img1[x, y] - img2[x, y]
+
+cv2.imshow('sub opencv', img3)
+cv2.moveWindow("sub opencv", img2.shape[1] + 10, img1.shape[0] + 40) 
+
+cv2.imshow('sub for unit8', img4)
+cv2.moveWindow("sub for unit8", 0, img1.shape[0] + 40) 
+
+for x in range(0, img1.shape[0]):
+    for y in range(0, img1.shape[1]):
+        if((int(img1[x, y]) - int(img2[x, y])) < 0):
+            img5[x, y] = 0
+        else:
+            img5[x, y] = int(img1[x, y]) - int(img2[x, y])
+
+cv2.imshow('sub for int', img5)
+cv2.moveWindow("sub for int", 0, img1.shape[0] *2 + 80) 
+
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
